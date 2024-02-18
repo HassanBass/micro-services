015e8c50a653578beb1f1b3d1acd26dc790501fe
TheCodeFather <107566331+HassanBass@users.noreply.github.com>
2024-02-18 09:29:48 +0200

    Update User.java

--- src/main/java/com/hassan/dev/microservices/entity/User.java
+++ src/main/java/com/hassan/dev/microservices/entity/User.java
@@ -26,7 +26,9 @@ public class User {
         this.id = id;
     }
 
-    
+    public String getName() {
+        return name;
+    }
 
     public void setName(String name) {
         this.name = name;
